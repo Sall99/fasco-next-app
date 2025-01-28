a69a3a0f5934bc46135c7e08699928f3d0c9a70a
Sall <sallaboudaouda@gmail.com>
2025-01-28 16:49:53 +0000

    add product details component with gallery, sharing options, and stock management

--- src/app/product/details/[id]/page.tsx
+++ src/app/product/details/[id]/page.tsx
@@ -1,12 +1,38 @@
 "use client";
-import { useGetProduct } from "@/actions";
 import { motion, AnimatePresence } from "framer-motion";
+import { Minus, Plus } from "lucide-react";
+import { FaTwitter, FaFacebookF } from "react-icons/fa";
+
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
+import { useGetProduct } from "@/actions";
+import { Button, Feature, Typography } from "@/components";
+import { StarRating } from "@/components/ui/star-rating";
 
 interface GalleryProps {
   images: string[];
+}
+
+interface DetailsProps {
+  name: string;
+  brand: string;
+  price: number;
+  viewersCount: number;
+  tags: string[];
+  description: string;
+  category: {
+    name: string;
+    slug: string;
+  };
+  stock: {
+    quantity: number;
+    lowStockThreshold: number;
+  };
+  rating: {
+    average: number;
+    reviewsCount: number;
+  };
 }
 
 function Gallery({ images }: GalleryProps) {
@@ -17,7 +43,7 @@ function Gallery({ images }: GalleryProps) {
   };
 
   return (
-    <div className="flex flex-col gap-4 md:flex-row md:gap-4">
+    <div className="flex flex-col justify-center gap-4 md:flex-row md:gap-4">
       <motion.div
         className="flex gap-2 overflow-x-auto md:max-h-[600px] md:flex-col md:overflow-y-auto"
         initial={{ opacity: 0, x: -20 }}
@@ -53,12 +79,12 @@ function Gallery({ images }: GalleryProps) {
       </motion.div>
 
       <motion.div
-        className="relative flex-1"
+        className="relative"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <div className="flex w-_438">
+        <div className="mx-auto flex h-full overflow-hidden md:w-_438 lg:h-_570">
           <AnimatePresence mode="wait">
             <motion.div
               key={mainIndex}
@@ -86,9 +112,152 @@ function Gallery({ images }: GalleryProps) {
   );
 }
 
+const Details = ({
+  name,
+  brand,
+  price,
+  viewersCount,
+  tags,
+  description,
+  category,
+  stock,
+  rating,
+}: DetailsProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrement = () => {
+    setQuantity(quantity - 1);
+  };
+
+  const handleTwitterShare = () => {
+    window.open(
+      `https://twitter.com/intent/tweet?text=${name}&url=${window.location.href}`,
+      "_blank",
+    );
+  };
+
+  const handleFacebookShare = () => {
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`,
+      "_blank",
+    );
+  };
+
+  return (
+    <div className="lg:w-_500">
+      <Typography variant="p-12" className="text-primary">
+        {category.name}
+      </Typography>
+      <Typography variant="p-12" className="text-primary">
+        {brand}
+      </Typography>
+      <Typography variant="h5" className="text-primary mb-4" font={"primary"}>
+        {name}
+      </Typography>
+      <div className="mb-4 flex items-center gap-2">
+        <StarRating average={rating.average} />
+        {rating.reviewsCount > 0 && (
+          <Typography variant="p-12" className="text-primary">
+            ({rating.reviewsCount} reviews)
+          </Typography>
+        )}
+      </div>
+      <Typography variant="h6" className="text-primary">
+        ${price}
+      </Typography>
+      <Typography variant="p-12" className="text-primary mt-4">
+        {description}
+      </Typography>
+
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag, index) => (
+          <Typography
+            key={index}
+            variant="p-12"
+            className="text-primary rounded-full bg-gray-200 px-2 py-1"
+          >
+            {tag}
+          </Typography>
+        ))}
+
+        <Typography
+          variant="p-12"
+          className="text-primary rounded-full bg-gray-200 px-2 py-1"
+        >
+          {category.slug}
+        </Typography>
+      </div>
+
+      <div className="mt-4 flex flex-wrap items-center gap-2">
+        <Typography variant="p-12" className="text-primary">
+          Stock:
+        </Typography>
+        <Typography variant="p-12" className="text-primary">
+          {stock.quantity}
+        </Typography>
+
+        <Typography variant="p-12" className="text-primary">
+          Low stock threshold:
+        </Typography>
+        <Typography variant="p-12" className="text-primary">
+          {stock.lowStockThreshold}
+        </Typography>
+
+        <Typography variant="p-12" className="text-primary">
+          Viewers:
+        </Typography>
+        <Typography variant="p-12" className="text-primary flex gap-2">
+          <span>{viewersCount}</span>
+          <span>are viewing this product right now</span>
+        </Typography>
+      </div>
+
+      <div className="mt-4 flex flex-col gap-2">
+        <div className="flex items-center gap-2">
+          <button
+            className="rounded-md bg-gray-600 px-4 py-2 text-white"
+            onClick={handleDecrement}
+          >
+            <Minus size={10} />
+          </button>
+          <div className="flex w-8 items-center justify-center">
+            <Typography variant="p-12" className="text-primary">
+              {quantity}
+            </Typography>
+          </div>
+          <button
+            className="rounded-md bg-gray-600 px-4 py-2 text-white"
+            disabled={stock.quantity === 0}
+            onClick={handleIncrement}
+          >
+            <Plus size={10} />
+          </button>
+        </div>
+        <Button size="sm" className="mt-4">
+          Add to cart
+        </Button>
+      </div>
+      <div className="mt-4 flex gap-4">
+        <Typography variant="p-12" className="text-primary">
+          Share:
+        </Typography>
+        <button className="text-primary" onClick={handleTwitterShare}>
+          <FaTwitter size={20} />
+        </button>
+        <button className="text-primary" onClick={handleFacebookShare}>
+          <FaFacebookF size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default function Page() {
   const { id } = useParams<{ id: string }>();
-
   const { data, isLoading, isError } = useGetProduct(id);
 
   if (isLoading) {
@@ -107,9 +276,27 @@ export default function Page() {
     );
   }
 
+  const product = data?.data;
+
   return (
-    <div className="mx-auto flex max-w-4xl flex-col-reverse gap-4 p-4 md:flex-row">
-      <Gallery images={data?.data.images || []} />
-    </div>
+    <section className="mx-auto mt-10">
+      <div className="flex flex-col justify-center gap-4 p-4 md:gap-8 lg:flex-row">
+        <Gallery images={product?.images || []} />
+        <Details
+          name={product?.name || ""}
+          brand={product?.brand || ""}
+          price={product?.price || 0}
+          viewersCount={product?.viewersCount || 0}
+          tags={product?.tags || []}
+          description={product?.description || ""}
+          category={product?.category || { name: "", slug: "" }}
+          stock={product?.stock || { quantity: 0, lowStockThreshold: 0 }}
+          rating={product?.rating || { average: 0, reviewsCount: 0 }}
+        />
+      </div>
+      <div className="my-40">
+        <Feature />
+      </div>
+    </section>
   );
 }
